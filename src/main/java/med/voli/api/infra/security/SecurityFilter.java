f3cc53df2d880431bf53cf889958f73becd9ab3d
Hygor Luciano <hygorluciano0@gmail.com>
2023-08-14 16:53:59 -0300

    criando um filter

---
+++ src/main/java/med/voli/api/infra/security/SecurityFilter.java
@@ -0,0 +1,32 @@
+package med.voli.api.infra.security;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperraToken(request);
+        
+        System.out.println(tokenJWT);
+        
+        filterChain.doFilter(request, response);
+}
+    
+    private String recuperraToken(HttpServletRequest request){
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null){
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Autorization!");
+        }
+        return authorizationHeader.replace("Bearer","");
+    }
+}
